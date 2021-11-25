76e52aafe89503ce3edea4fff89ebb604250bec0
kwish <kwish@snu.ac.kr>
2021-11-25 13:29:49 +0900

    기술 스택 의존성

--- Wafflestagram/app/src/main/java/com/wafflestudio/wafflestagram/ui/login/LoginActivity.kt
+++ Wafflestagram/app/src/main/java/com/wafflestudio/wafflestagram/ui/login/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.wafflestudio.wafflestagram.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.wafflestudio.wafflestagram.R
+import androidx.appcompat.app.AppCompatActivity
+import com.wafflestudio.wafflestagram.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //login 버튼 활성화
+
+        //login 버튼 클릭 이벤트
+
+
     }
 }
