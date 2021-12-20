00abfdedde353df5f91128f30b3bf648ce14e7d1
kwish <kwish@snu.ac.kr>
2021-12-20 14:01:03 +0900

    로그인, 회원가입

---
+++ Wafflestagram/app/src/main/java/com/wafflestudio/wafflestagram/ui/signup/SignUpActivity.kt
@@ -0,0 +1,59 @@
+package com.wafflestudio.wafflestagram.ui.signup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import com.wafflestudio.wafflestagram.databinding.ActivitySignUpBinding
+import com.wafflestudio.wafflestagram.ui.login.LoginActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+    private val fragmentSignupByPhoneFragment = SignupByPhoneFragment()
+    private val fragmentSignupByEmailFragment = SignupByEmailFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setFragment(0)
+        binding.layoutTab.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab!!.position){
+                    0 -> setFragment(0)
+                    1 -> setFragment(1)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+
+        })
+
+        binding.buttonLogin.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setFragment(fragmentNum : Int){
+        val fb = supportFragmentManager.beginTransaction()
+        when(fragmentNum){
+            0 -> {
+                fb.replace(binding.fragmentContainerViewSignup.id, fragmentSignupByPhoneFragment).commit()
+            }
+            1 ->{
+                fb.replace(binding.fragmentContainerViewSignup.id, fragmentSignupByEmailFragment).commit()
+            }
+        }
+    }
+}
---
+++ Wafflestagram/app/src/main/java/com/wafflestudio/wafflestagram/ui/signup/SignUpDetailActivity.kt
@@ -0,0 +1,66 @@
+package com.wafflestudio.wafflestagram.ui.signup
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import com.wafflestudio.wafflestagram.databinding.ActivitySignUpDetailBinding
+import com.wafflestudio.wafflestagram.ui.main.MainActivity
+
+class SignUpDetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignUpDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.editName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                if(p0?.length!! > 0 && binding.editPassword.text.length >= 6){
+                    binding.buttonContinue.isClickable = true
+                    binding.buttonContinue.isEnabled = true
+                    binding.buttonContinue.setTextColor(Color.parseColor("#FFFFFFFF"))
+                }else{
+                    binding.buttonContinue.isClickable = false
+                    binding.buttonContinue.isEnabled = false
+                    binding.buttonContinue.setTextColor(Color.parseColor("#EAEAEA"))
+                }
+            }
+
+        })
+        binding.editPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                if(p0?.length!! >= 6 && binding.editName.text.isNotEmpty()){
+                    binding.buttonContinue.isClickable = true
+                    binding.buttonContinue.isEnabled = true
+                    binding.buttonContinue.setTextColor(Color.parseColor("#FFFFFFFF"))
+                }else{
+                    binding.buttonContinue.isClickable = false
+                    binding.buttonContinue.isEnabled = false
+                    binding.buttonContinue.setTextColor(Color.parseColor("#EAEAEA"))
+                }
+                if(p0?.length!! < 6){
+                    binding.textInputLayoutPassword.error = "비밀번호는 6자 이상이어야 합니다."
+                }else{
+                    binding.textInputLayoutPassword.error = null
+                }
+            }
+
+        })
+
+        binding.buttonContinue.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
