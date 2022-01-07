afbe2a8c68cc910681c5a6e4bc6bfc9debf450e2
B0neh3ad <js1044k@gmail.com>
2022-01-07 13:04:44 +0900

    Connect login(email and password) with server

---
+++ Wafflestagram/app/src/main/java/com/wafflestudio/wafflestagram/network/dto/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.wafflestudio.wafflestagram.network.dto
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
--- Wafflestagram/app/src/main/java/com/wafflestudio/wafflestagram/network/dto/LoginResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.wafflestudio.wafflestagram.network.dto
-
-data class LoginResponse(
-    val status: Int,
-    val token: String?,
-    val message: String
-)
